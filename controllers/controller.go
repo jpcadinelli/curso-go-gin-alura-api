f2ba10ae7e64f2a05fca1d6fba0e33edb9598307
João Pedro Cadinelli <62340339+jpcadinelli@users.noreply.github.com>
2024-04-03 12:47:06 -0300

    Aula 3 Finalizada

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jpcadinelli/curso-go-gin-alura-api/database"
 	"github.com/jpcadinelli/curso-go-gin-alura-api/models"
 )
 
@@ -14,4 +17,14 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz:": "E aí " + nome + ", tudo beleza?",
 	})
+}
+func CriaNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 }
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/jpcadinelli/curso-go-gin-alura-api/database"
 	"github.com/jpcadinelli/curso-go-gin-alura-api/models"
 	"github.com/jpcadinelli/curso-go-gin-alura-api/routes"
 )
 
 func main() {
+	database.ConectaComBancoDeDados()
 	models.Alunos = []models.Aluno{
 		{Nome: "João Pedro", CPF: "000.000.000-00", RG: "00.000.000-0"},
 		{Nome: "Gui Lima", CPF: "111.111.111-11", RG: "11.111.111-1"},
